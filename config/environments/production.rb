bb2ab87cdf0a337f8f11e0aeb9624b5d08121910
philmphoenix <phillip.matthew.phoenix@gmail.com>
2014-12-21 00:51:25 -0700

    Homepage template is about halfway done

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # For Devise
+  config.action_mailer.default_url_options = { :host => 'http://bookamonthclub.herokuapp.com' }
+
 end
